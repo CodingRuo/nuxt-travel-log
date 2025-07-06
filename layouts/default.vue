b495f4a7e1c2cd1922b3674da4f79b0438c7e0c6
René Schober <r.schober@outlook.com>
2025-07-06 16:34:29 +0200

    :sparkles: add landing page

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang=ts>
+
+</script>
+
+<template>
+  <div class="flex h-screen flex-col overflow-hidden">
+    <AppNavBar />
+    <main class="flex-1 overflow-hidden">
+      <slot />
+    </main>
+  </div>
+</template>
