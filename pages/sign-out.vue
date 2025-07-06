a9bd1391994d664ddb83ed52fbe58f70b45034f0
René Schober <r.schober@outlook.com>
2025-07-06 21:24:05 +0200

    :sparkles: adding logout and redirect (#56)

---
+++ pages/sign-out.vue
@@ -0,0 +1,16 @@
+<script setup lang=ts>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card container mt-4 min-h-screen mx-auto flex flex-col justify-center items-center gap-4">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
+
+<style scoped>
+</style>
