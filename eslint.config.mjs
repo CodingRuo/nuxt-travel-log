9fb691a5d8a6ec1999e8527e06422de3795f18fb
René Schober <r.schober@outlook.com>
2025-07-06 15:17:03 +0200

    change es lint rules, and setup github actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,29 +4,29 @@ import antfu from "@antfu/eslint-config";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(antfu({
-    type: "app",
-    vue: true,
-    typescript: true,
-    formatters: true,
-    stylistic: {
-        indent: 4,
-        semi: true,
-        quotes: "double",
-    },
+  type: "app",
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
 }, {
-    rules: {
-        "ts/no-redeclare": "off",
-        "ts/consistent-type-definitions": ["error", "type"],
-        "no-console": ["warn"],
-        "antfu/no-top-level-await": ["off"],
-        "node/prefer-global/process": ["off"],
-        "node/no-process-env": ["error"],
-        "perfectionist/sort-imports": ["error", {
-            tsconfigRootDir: ".",
-        }],
-        "unicorn/filename-case": ["error", {
-            case: "kebabCase",
-            ignore: ["README.md"],
-        }],
-    },
+  rules: {
+    "ts/no-redeclare": "off",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
 }));
